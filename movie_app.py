94f87d61dde7d7ce152928277e16021127ace7d0
Chris <chris.lescinskas@gmail.com>
2023-03-30 00:36:56 +0000

    Merged app routes. Added search route. Added sending moviename to flask

--- movie_app.py
+++ movie_app.py
@@ -12,7 +12,7 @@
 
 import prefix
 import sqlite3
-from flask import Flask, url_for, render_template
+from flask import Flask, url_for, render_template, request
 from markupsafe import escape
 
 
@@ -27,28 +27,27 @@ c = conn.cursor()
 # Calling this routine will have no effect if running on local machine
 prefix.use_PrefixMiddleware(app)   
 
-# THIS IS A DUMMY INDEX PAGE
-# TODO - Chris I don't know if we want to add my route to your file or yours to this, we can figure that out
 @app.route('/')  
-def index():
-    resp =  ''' 
-            <html>
-            <link rel="stylesheet" href="static/css/myflaskapp.css">
-             <div>
-                 Watchawatchin?!
-            </div><br><hr><br>
-            Top Selling Movie:
-             '''
-    # TODO - We will need to figure out how to handle passing the searched movie into the moviename variable
-    movie_url = url_for("show_movie_profile", moviename='Twilight')
-    resp +=  '<a href="' + movie_url + '" > Twilight</a>'
-    return resp
+def homepage():
+    fetch = "SELECT genre1 FROM titleBasics LIMIT 4"
+    c.execute(fetch)
+    data = c.fetchall()
+    return render_template('whatchawatchin.html', data=data)
 
-# THIS IS THE ROUTE FOR THE MOVIE DETAILS PAGE
-# TODO - How to use this route from another page, probably the movie category page
-# TODO - Need to pass a user selection into <moviename>
-@app.route('/<moviename>')
-def show_movie_profile(moviename):
+
+
+#This is meant to be a dummy page to explore passing variable into route
+#Eventually this will be replaced by genre 
+@app.route('/search/')
+def search_movie():
+    return render_template("search.html")
+
+
+@app.route('/search/movie/', methods=['POST', 'GET'])
+def show_movie_profile():
+    moviename = request.form['movie_name']
+    ## How do we handle movies that aren't in databse?
+    ## How do we handle movies with the same name?
     fetch = "SELECT * FROM titleBasics WHERE primaryTitle = '" + moviename + "'" 
     c.execute(fetch)
     data = c.fetchall()
