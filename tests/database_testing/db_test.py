7e3bb0072e9ebb4c6265dd484e85432240e49caf
dudeilovemae <rdrfranco@gmail.com>
2023-04-26 19:58:50 -0600

    Added logic for login test 2

--- tests/database_testing/db_test.py
+++ tests/database_testing/db_test.py
@@ -19,31 +19,24 @@ import sqlite3
 dbAPI.addLike("ray", 1234, "../../movie_app.db")
 dbAPI.addLike("ray", 8460, "../../movie_app.db")
 dbAPI.addLike("ray", 7888, "../../movie_app.db")
-
-
 dbAPI.removeLike("ray", 7888, "../../movie_app.db")
-
-
 dbAPI.countLikesByUser("ray", "../../movie_app.db")
-
 dbAPI.countLikesByMovie(7888, "../../movie_app.db")
 
 testdb = "my_test.db"
 
 class WidgetTestCase(unittest.TestCase):
     def setUp(this):
-        print("Creating test database called my_test.db")
         dbCreate.create(testdb)
-        print("...loading my_test.db")
         dbCreate.fill(testdb)
 
     def tearDown(this): 
-        print("Removing my_test.db")
         os.remove('my_test.db')
+
 
     ## Login test 1: Check for table
     ## Description: Check to see if database contains table for user information
-    def test1(self):
+    def test_login_1(self):
         # we should be able to see if the table exists if we query it
         conn = sqlite3.connect(testdb)
         c = conn.cursor()
@@ -54,14 +47,33 @@ class WidgetTestCase(unittest.TestCase):
         conn.commit()
         conn.close()
 
-        print(ret)
         self.assertEqual(ret[0][0], 'ping')
 
 
     ## Login test 2: Adding User Valid Criteria Check 
     ## Description: Create a user with a valid user name and password, and add to login information table
-    def test2(self):
-         print("todo")
+    def test_login_2(self):
+         ## Valid Username and password
+         self.assertTrue(dbAPI.addUser('ray', '12345678', testdb))
+         ## username is too short
+         self.assertFalse(dbAPI.addUser('ra', '12345678', testdb))
+         ## password is too short
+         self.assertFalse(dbAPI.addUser('jake', '1234567', testdb))
+         ## Username and password is too short
+         self.assertFalse(dbAPI.addUser('ja', '12', testdb))
+         ## Username cannot have a space in it
+         self.assertFalse(dbAPI.addUser('jake doe', 'this_password_is_ok', testdb))
+         ## Password cannot have a space in it
+         self.assertFalse(dbAPI.addUser('hello', 'not okay', testdb))
+
+         ## For fun let's add some more valid users
+         self.assertTrue(dbAPI.addUser('tim', 'password', testdb))
+         self.assertTrue(dbAPI.addUser('lilly', 'password123', testdb))
+         self.assertTrue(dbAPI.addUser('megaman', 'password_!!', testdb))
+
+         ## Usernames cannot be the same
+         self.assertFalse(dbAPI.addUser('tim', 'diff_password', testdb))
+         self.assertFalse(dbAPI.addUser('megaman', 'password_!!', testdb))
 
 if __name__ == '__main__':
     unittest.main()
