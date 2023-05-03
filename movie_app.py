4d75a0ff28cf71edf01056018db45fbea098ecaa
fraykate <34926259+fraykate@users.noreply.github.com>
2023-05-03 15:52:02 -0600

    Update movie_app.py

--- movie_app.py
+++ movie_app.py
@@ -29,7 +29,7 @@ c = conn.cursor()
 
 # Insert the wrapper for handling PROXY when using csel.io virtual machine
 # Calling this routine will have no effect if running on local machine
-prefix.use_PrefixMiddleware(app)   
+# prefix.use_PrefixMiddleware(app)   
 
 @app.route('/')  
 def homepage():
